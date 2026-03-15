a5a786d0805e57ed4542a2762b81c42b013227ec
Your Name <you@example.com>
2026-03-16 01:07:59 +0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1 class="text-3xl font-bold underline text-pink-500">
+  Hello world!
+</h1>
+
+<style lang="postcss">
+  @reference "tailwindcss";
+  :global(html) {
+    background-color: theme(--color-gray-100);
+  }
+</style>
